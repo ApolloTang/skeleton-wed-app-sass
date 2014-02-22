122b7ae2e5ea0623c63e3be34b4e7c41fee5eae6
Apollo Tang <tang.apollo@gmail.com>
2014-02-22 18:20:30 -0500

    now using require.js

---
+++ js/main.js
@@ -0,0 +1,45 @@
+﻿requirejs.config({
+    paths: {
+        jquery: 'libs/jquery-1.9.0'
+        , underscore: 'libs/underscore-1.4.4'
+    }
+    , urlArgs: "bust=" + (new Date()).getTime()
+    , shim: {
+        'jquery': {
+            exports: '$'
+        }
+        , 'underscore': {
+            exports: '_',
+            deps: ['jquery']
+        }
+    }
+});
+
+
+
+require(
+    ['jquery', 'underscore'],
+    function ($, _) {
+    
+        // resolve the asp.net/underscore conflict for < % tags
+        _.templateSettings = {
+            interpolate: /\{\{(.+?)\}\}/gim,  // print value: {{ value_name }}
+            evaluate: /\{%([\s\S]+?)%\}/gim,  // excute code: {% code_to_execute %}
+            escape: /\{%-([\s\S]+?)%\}/gim    // excape HTML: {%- <script> %} prints &lt;script&gt;
+        };
+  
+        //
+        function delayExecute($, _, ui) {  debugger; }
+            
+        $(document).ready( function ($, _) {
+             debugger;
+            require( ['modules/ui'],  function (ui) {
+                        debugger;
+            });
+            // setTimeout( delayExecute, 2000 );
+
+        });
+
+
+    }
+);
---
+++ js/modules/ui.js
@@ -0,0 +1,137 @@
+define(['jquery'], 
+function ($) {
+
+   
+
+	var ui = {};
+	ui.getViewPortDim =  function(){ return { 
+				w: $(window).width(),
+				h: $(window).height() };
+		};
+	ui.getDocDim =  function(){ return { 
+				w: $(document).width(),
+				h: $(document).height() };
+		};
+
+	var mouseIsPressed = false;
+	var mousePressedTimeOut;
+    var workspace_content_minimum_w = 1000;	
+	
+
+
+	function adjustLayout( viewPortDim ) {
+		
+		/* vertical adjustment */
+		var skeletalHeader_h = $("#skeletal_head").height();
+		var skeletalFooter_h = $("#skeletal_foot").height();
+		var tabContainer_h   = $("#tab_container").height();
+ 		var skeletalBody_h = viewPortDim.h - skeletalHeader_h - skeletalFooter_h;
+		var workspace_h = skeletalBody_h - tabContainer_h;	
+		$("#skeletal_body").css({'height': skeletalBody_h });
+	    $("#workspace").css({'height': workspace_h });
+	    
+	    /* horizontal adjustment */
+	    $("#skeletal_main").css({'width': viewPortDim.w});
+		var skeletal_main_inner_w = viewPortDim.w - $("#grip_main").width();
+		var workspace_content_w = skeletal_main_inner_w - $("#workspace_tool_bar").width();
+	    $("#workspace_content").css({'width': workspace_content_w});	
+	    $("#skeletal_main_inner").css({'width': skeletal_main_inner_w});
+		var workspace_content_inner_w = ( workspace_content_w > workspace_content_minimum_w) 
+							? workspace_content_w
+							: workspace_content_minimum_w;
+		$("#workspace_content_inner").css({'width': workspace_content_inner_w});					
+	} /* end-func djustLayout */
+
+	
+	/* monitor key press */
+	$("body").keydown(function(e) {
+		var skeletal_main_left = $("#skeletal_main").offset().left;	
+	
+	  if(e.keyCode == 37) { // left
+	  	skeletal_main_left = skeletal_main_left -10;    
+		$("#skeletal_main").css({'left': skeletal_main_left });
+		$("#skeletal_sidebar").width(skeletal_main_left);
+		adjustLayout( ui.getViewPortDim() );
+	  }
+	  else if(e.keyCode == 39) { // right
+	  	skeletal_main_left = skeletal_main_left + 10; 
+		$("#skeletal_main").css({'left': skeletal_main_left});
+		$("#skeletal_sidebar").width(skeletal_main_left);
+		adjustLayout( ui.getViewPortDim() );
+	  }
+	});
+	
+
+    $("#button_to_slide_main").click(function(){
+    	var skeletal_main_left = $("#skeletal_main").offset().left; 	
+		if (skeletal_main_left > 0) {
+			$("#skeletal_main").animate({'left': 0});
+			$("#skeletal_sidebar").animate({'width': 0});
+		
+			$("#button_to_slide_main").html('&gt;');
+		}
+		if (skeletal_main_left == 0) {
+			$("#skeletal_main").animate({'left': 500});
+			$("#button_to_slide_main").html('&lt;');
+			$("#skeletal_sidebar").animate({'width': 500});
+		}
+    });
+	
+	/* mornitor mouse pressed	 */
+	$("#grip_main").mousedown(function(e) {
+		e.preventDefault();
+	    mouseIsPressed = true;
+	    mousePressedTimeOut = setTimeout( GripMainIsOn, 50);
+	    document.body.style.cursor = 'col-resize';
+	})
+	$(document).bind('mouseup', function() {
+	    mouseIsPressed = false;
+		GripMainIsOff();
+	    clearTimeout(mousePressedTimeOut);
+	    document.body.style.cursor = '';
+	});
+		
+	function GripMainIsOn() {
+		$(document).mousemove( function(e){
+			if(mouseIsPressed) {
+				appConsole('GripMainIsON called');
+				// read mouse coordinate
+        		$("#console1").text(e.pageX + ', ' + e.pageY );
+        				$("#skeletal_main").css({'left': e.pageX});
+        				$("#skeletal_sidebar").width(e.pageX);
+		
+        	}
+        } );		
+		$("#grip_main").addClass('gripOn');
+	};
+	function GripMainIsOff() {
+		appConsole('GripMainIsOff called');
+		$("#grip_main").removeClass('gripOn');
+		var skeletal_main_left = $("#skeletal_main").offset().left; 	
+		if (skeletal_main_left > 0) {
+ 			$("#button_to_slide_main").html('&lt;');
+		}
+	};
+
+	function appConsole( message ) {
+		$("#console1").prepend( message + '</br>');
+	}   
+    
+    debugger;
+    /* layout monitoring */ 
+	adjustLayout( ui.getViewPortDim() );
+	$(window).resize( function() { adjustLayout( ui.getViewPortDim() ); });
+    
+    ui.showJob = function() {
+    	    	var skeletal_main_left = $("#skeletal_main").offset().left; 	
+		if (skeletal_main_left == 0) $("#skeletal_main").animate({'left': 500});
+    }
+    ui.hideJob = function() {
+    	    	var skeletal_main_left = $("#skeletal_main").offset().left; 	
+		if (skeletal_main_left > 0) $("#skeletal_main").animate({'left': 0});
+    }
+         
+        
+    
+return ui;
+});
